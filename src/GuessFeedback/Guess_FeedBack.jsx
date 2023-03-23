aa45c3ed370c5a7696901aef1963bec82ecb6f1d
Brian Ho <brian@MacBook-Pro.local>
2023-03-22 19:04:54 -0700

    modify color display

--- src/GuessFeedback/Guess_FeedBack.jsx
+++ src/GuessFeedback/Guess_FeedBack.jsx
@@ -1,44 +1,43 @@
-import React from "react";
 import { Box, Typography } from "@mui/material";
+import "./Guess_Feedback.css";
 
 function GuessFeedback(props) {
-  const {feedback, typedWords, guessCount} = props;
+  const { feedback, typedWords, guessCount } = props;
 
   return (
     <Box>
-      <Box className="feedback">
+      {/* <Box className="feedback">
         {feedback.map((item, index) => (
           <Typography
             key={index}
             variant="body1"
             className={
               item === "Correct"
-                ? "correct"
+                ? "correct feedback-item"
                 : item === "Wrong"
-                ? "wrong"
-                : "close"
+                ? "wrong feedback-item"
+                : "close feedback-item"
             }
             style={{ margin: "0 4px" }}
           >
             {item}
           </Typography>
         ))}
-      </Box>
+      </Box> */}
       <Box>
         <Typography variant="h6" component="h6">
           Guessed Words
         </Typography>
-        <Typography variant="body2" >
-          Guessed Count : {guessCount}
-        </Typography>
-        {typedWords.map((item, index) => (
-          <Typography key={index} variant="body1" style={{ margin: "0 4px" }}>
-            {item}
+        <Typography variant="body2">Guessed Count : {guessCount}</Typography>
+        {typedWords.map((word, wordIndex) => (
+          <Typography key={wordIndex} variant="body1">
+            {word}
           </Typography>
         ))}
       </Box>
     </Box>
   );
 }
+
 
 export default GuessFeedback;
